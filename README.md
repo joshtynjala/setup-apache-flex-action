2ba10bd674f3a1afa32ff4900a3b574e8a45cde9
Josh Tynjala <joshtynjala@bowlerhat.dev>
2022-11-14 15:13:36 -0800

    start refactoring for Harman AIR

--- README.md
+++ README.md
@@ -6,12 +6,44 @@ This action downloads the [Apache Flex SDK](https://flex.apache.org/), adds the 
 
 ### `flex-version`
 
-_(Optional)_ Version of the Apache Flex SDK. This value must include major, minor, and revision parts, such as `4.16.1`.
+_(Required)_ Version of the Apache Flex SDK. This value must include major, minor, and revision parts, such as `4.16.1`.
+
+### `air-version`
+
+_(Required)_ The version of the Adobe AIR SDK to set up. An exact version, such as `50.0.1.1`, is recommended. However, a less specific version, like `50.0` or `50`, is allowed.
+
+### `accept-air-license`
+
+_(Required)_ Set to `true` if you accept the [Adobe AIR SDK License Agreement](https://airsdk.harman.com/assets/pdfs/HARMAN%20AIR%20SDK%20License%20Agreement.pdf). The action will fail if the agreement is not accepted.
+
+### `air-license-base64`
+
+_(Optional)_ Use with an [encrypted secret](https://docs.github.com/en/actions/security-guides/encrypted-secrets) to optionally provide a valid _adt.lic_ file, [encoded as a Base64 string](https://docs.github.com/en/actions/security-guides/encrypted-secrets#storing-base64-binary-blobs-as-secrets), to unlock new, restricted features.
+
+**Warning!** Never include the raw Base64-encoded string value directly in your Github Actions _.yml_ file. You **must** use an [encrypted secret](https://docs.github.com/en/actions/security-guides/encrypted-secrets) to prevent your license file from being leaked publicly.
 
 ## Example usage
 
+Use the Apache Flex SDK with the AIR SDK by HARMAN for free activities:
+
 ```yml
-uses: joshtynjala/setup-apache-flex-action@v1
+uses: joshtynjala/setup-apache-flex-action@v2
 with:
   flex-version: "4.16.1"
+  air-version: "50.0.1.1"
+  accept-air-license: true
 ```
+
+Specify an encypted secret containing a Base64-encoded license file to unlock new, restricted features:
+
+```yml
+uses: joshtynjala/setup-apache-flex-action@v2
+with:
+  flex-version: "4.16.1"
+  air-version: "50.0.1.1"
+  accept-air-license: true
+  air-license-base64: ${{ secrets.AIR_SDK_LICENSE_FILE }}
+```
+
+
+
--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const core = require("@actions/core");
 const toolCache = require("@actions/tool-cache");
 const child_process = require("child_process");
 const fs = require("fs");
+const os = require("os");
 const path = require("path");
 
 const sdkConfigParseErrorText =
@@ -90,7 +91,7 @@ function getFlexSDKProducts(sdkConfigXML) {
   return apacheFlexXML;
 }
 
-function getVersion(expectedVersion, apacheFlexXML) {
+function getFlexVersionBestMatch(/** @type string */ expectedVersion, apacheFlexXML) {
   if (!apacheFlexXML || !("children" in apacheFlexXML)) {
     throw new Error(sdkConfigParseErrorText);
   }
@@ -111,15 +112,59 @@ function getVersion(expectedVersion, apacheFlexXML) {
     throw new Error(sdkConfigParseErrorText);
   }
 
-  return versionLettersXML.find((versionLetterXML) => {
-    return (
-      "attributes" in versionLetterXML &&
-      versionLetterXML.attributes.version === expectedVersion
-    );
-  });
+  let bestMatch = null;
+  const requestedParts = expectedVersion.split(".");
+  for (let releaseXML of versionLettersXML) {
+    if (!("attributes" in releaseXML)) {
+      continue;
+    }
+    const releaseVersion = releaseXML.attributes.version;
+    const releaseParts = releaseVersion.split(".");
+    let matched = true;
+    for (let i = 0; i < requestedParts.length; i++) {
+      if (requestedParts[i] != releaseParts[i]) {
+        matched = false;
+        break;
+      }
+    }
+    if (matched) {
+      // this assumes that the releases are in order from newest to oldest
+      bestMatch = releaseVersion;
+      break;
+    }
+  }
+  if (bestMatch == null) {
+    throw new Error(`Apache Flex SDK version '${expectedVersion}' not found`);
+  }
+  return bestMatch;
 }
 
-function getVersionLetterURL(versionLetterXML, mirrorPrefix) {
+function getAIRVersionBestMatch(/** @type string */ airVersion, /** @type {any[]} */ releases) {
+  let bestMatch = null;
+  const requestedParts = airVersion.split(".");
+  for (let release of releases) {
+    const releaseName = release.name;
+    const releaseParts = releaseName.split(".");
+    let matched = true;
+    for (let i = 0; i < requestedParts.length; i++) {
+      if (requestedParts[i] != releaseParts[i]) {
+        matched = false;
+        break;
+      }
+    }
+    if (matched) {
+      // this assumes that the releases are in order from newest to oldest
+      bestMatch = releaseName;
+      break;
+    }
+  }
+  if (bestMatch == null) {
+    throw new Error(`Adobe AIR SDK (HARMAN) version '${airVersion}' not found`);
+  }
+  return bestMatch;
+}
+
+function getFlexVersionLetterURL(versionLetterXML, mirrorPrefix) {
   let url = `${versionLetterXML.attributes.path}${versionLetterXML.attributes.file}`;
   if (!/^https?:\/\//.test(url)) {
     url = `${mirrorPrefix}/${url}`;
@@ -138,65 +183,102 @@ function getVersionLetterURL(versionLetterXML, mirrorPrefix) {
 
 async function setupApacheFlex() {
   try {
-    let flexVersion = core.getInput("flex-version");
-    if (flexVersion && !/^\d+\.\d+.\d+$/.test(flexVersion)) {
-      throw new Error(`Invalid Apache Flex version: ${flexVersion}`);
-    }
-
-    const sdkConfigXML = await loadSDKConfig();
-    const apacheFlexXML = getFlexSDKProducts(sdkConfigXML);
-
-    if (!flexVersion) {
-      flexVersion = apacheFlexXML.attributes.latestVersion;
-    }
-    console.log("Apache Flex version: " + flexVersion);
-
-    const mirrorURLPrefix = await getMirrorURLPrefix(sdkConfigXML);
-
-    const flexDownloadURL = getVersionLetterURL(
-      getVersion(flexVersion, apacheFlexXML),
-      mirrorURLPrefix
-    );
-
-    const flexDownloadFileName = path.basename(
-      new URL(flexDownloadURL).pathname
-    );
-    const downloadedPath = await toolCache.downloadTool(
-      flexDownloadURL,
-      flexDownloadFileName
-    );
-
-    let installLocation = process.platform.startsWith("win")
-      ? "c:\\ApacheFlexSDK"
-      : "/usr/local/bin/ApacheFlexSDK";
-    fs.mkdirSync(installLocation);
-
-    if (process.platform.startsWith("darwin")) {
-      await toolCache.extractTar(downloadedPath, installLocation);
-    } else if (process.platform.startsWith("win")) {
-      await toolCache.extractZip(downloadedPath, installLocation);
-    }
-
-    let flexHome = installLocation;
-    if (process.platform.startsWith("darwin")) {
-      const baseFileName = flexDownloadFileName.substr(
-        0,
-        flexDownloadFileName.length - 7 //.tar.gz
+    const acceptAIRLicense = core.getInput("accept-air-license", { required: true });
+    if (!acceptAIRLicense) {
+      throw new Error(
+        "Parameter `accept-air-license` must be true to accept the Adobe AIR SDK License Agreement. Find it here: https://airsdk.harman.com/assets/pdfs/HARMAN%20AIR%20SDK%20License%20Agreement.pdf"
       );
-      flexHome = path.resolve(installLocation, baseFileName);
     }
-    core.addPath(path.resolve(flexHome, "bin"));
-    core.exportVariable("FLEX_HOME", flexHome);
+    const licenseFile = core.getInput("air-license-base64", { required: false });
+    if (licenseFile) {
+      const licenseBuffer = Buffer.from(licenseFile, "base64");
+      const licensePath = path.join(os.homedir(), ".airsdk", "adt.lic");
+      fs.mkdirSync(path.dirname(licensePath), { recursive: true });
+      fs.writeFileSync(licensePath, licenseBuffer);
+    }
 
-    child_process.execSync(
-      "ant -f installer.xml -Dflash.sdk.version=32.0 -Dair.sdk.version=32.0 -Dinstaller=true -Ddo.flash.install=1 -Ddo.air.install=1 -Ddo.swfobject.install=1 -Ddo.fontswf.install=1 -Ddo.osmf.install=1 -Ddo.ofl.install=1",
-      {
-        cwd: flexHome,
-        stdio: "inherit",
-      }
-    );
+    let flexVersion = core.getInput("flex-version", { required: true });
+    const sdkConfigXML = await loadSDKConfig();
+    const mirrorURLPrefix = await getMirrorURLPrefix(sdkConfigXML);
+    const apacheFlexXML = getFlexSDKProducts(sdkConfigXML);
+    flexVersion = getFlexVersionBestMatch(flexVersion, apacheFlexXML);
+
+    console.log("Apache Flex SDK version: " + flexVersion);
+
+    const flexHome = await downloadFlexSDK(flexVersion, mirrorURLPrefix);
+
+    const airVersion = core.getInput("air-version", { required: true });
+    const parsedMajorVersion = parseInt(airVersion.split(".")[0], 10);
+    if (parsedMajorVersion <= 32) {
+      // try to set up an old Adobe version of the AIR SDK
+      setupApacheFlexWithAdobeAIR(airVersion, flexHome);
+      return;
+    }
+    await setupApacheFlexWithHarmanAIR(airVersion, flexHome);
   } catch (error) {
     core.setFailed(error.message);
   }
 }
+
+async function downloadFlexSDK(/** @type string */ flexVersion, /** @type string */ mirrorURLPrefix) {
+  const flexDownloadURL = getFlexVersionLetterURL(
+    flexVersion,
+    mirrorURLPrefix
+  );
+
+  const flexDownloadFileName = path.basename(
+    new URL(flexDownloadURL).pathname
+  );
+  const downloadedPath = await toolCache.downloadTool(
+    flexDownloadURL,
+    flexDownloadFileName
+  );
+
+  const installLocation = process.platform.startsWith("win")
+    ? "c:\\ApacheFlexSDK"
+    : "/usr/local/bin/ApacheFlexSDK";
+  fs.mkdirSync(installLocation);
+
+  if (process.platform.startsWith("darwin")) {
+    await toolCache.extractTar(downloadedPath, installLocation);
+  } else if (process.platform.startsWith("win")) {
+    await toolCache.extractZip(downloadedPath, installLocation);
+  }
+
+  let flexHome = installLocation;
+  if (process.platform.startsWith("darwin")) {
+    const baseFileName = flexDownloadFileName.substr(
+      0,
+      flexDownloadFileName.length - 7 //.tar.gz
+    );
+    flexHome = path.resolve(installLocation, baseFileName);
+  }
+  core.addPath(path.resolve(flexHome, "bin"));
+  core.exportVariable("FLEX_HOME", flexHome);
+  return flexHome;
+}
+
+async function setupApacheFlexWithHarmanAIR(/** @type string */ airVersion, /** @type string */ flexHome) {
+  throw new Error("Adobe AIR by HARMAN not yet supported");
+}
+
+function setupApacheFlexWithAdobeAIR(/** @type string */ airVersion, /** @type string */ flexHome) {
+  if (airVersion == "32") {
+    airVersion += ".0";
+  }
+  if (airVersion != "32.0") {
+    throw new Error(
+      `Expected Adobe AIR major version 32 or newer. Received version: ${airVersion}`
+    );
+  }
+
+  child_process.execSync(
+    "ant -f installer.xml -Dflash.sdk.version=32.0 -Dair.sdk.version=32.0 -Dinstaller=true -Ddo.flash.install=1 -Ddo.air.install=1 -Ddo.swfobject.install=1 -Ddo.fontswf.install=1 -Ddo.osmf.install=1 -Ddo.ofl.install=1",
+    {
+      cwd: flexHome,
+      stdio: "inherit",
+    }
+  );
+}
+
 setupApacheFlex();
